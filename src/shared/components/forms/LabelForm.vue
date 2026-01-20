1ceef986681fc27a8c9dd49da03ac150eecfaa39
Sahid Alejandro <sahidaledev@gmail.com>
2026-01-20 16:05:58 -0600

    feat(): integrar los componentes de texto en varias vistas

--- src/shared/components/forms/LabelForm.vue
+++ src/shared/components/forms/LabelForm.vue
@@ -5,6 +5,7 @@ import { computed } from 'vue';
 interface Props {
   size?: 'xs' | 'sm' | 'lg';
   for: string;
+  description?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -22,7 +23,12 @@ const sizeClasses = computed(() => {
 </script>
 
 <template>
-  <label :for="props.for" class="block text-gray-900 dark:text-white" :class="sizeClasses">
-    <slot></slot>
+  <label :for="props.for" class="block text-gray-900 dark:text-white">
+    <div :class="sizeClasses">
+      <slot></slot>
+    </div>
+    <div v-if="description" class="text-xs font-normal text-gray-600 dark:text-gray-400 mt-0.5">
+      {{ description }}
+    </div>
   </label>
 </template>
