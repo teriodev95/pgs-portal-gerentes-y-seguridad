4ffd42a95dcc49e4fbf2fc39d0d96bde2e2e698f
Sahid Alejandro <sahidaledev@gmail.com>
2026-02-11 13:56:40 -0600

    feat: cambiar endpont para obtener incidentes por usuario, de sfast a fax

--- src/features/incident/types/incident.types.ts
+++ src/features/incident/types/incident.types.ts
@@ -1,15 +1,31 @@
 export interface IIncidentFormData {
   categoria: 'incidente' | 'reposicion' | 'nomina'
-  tipo: 'ingreso' | 'egreso'
   comentario: string
   monto: number
+  tipo: 'ingreso' | 'egreso'
+}
+
+export interface IUser {
+  usuarioid: number;
+  nombre: string;
+  apellidoPaterno: string;
+  apellidoMaterno: string;
+  usuario: string;
+  tipo: string;
+  gerencia: string;
+  agencia: string | null;
 }
 
 export interface IIncident extends IIncidentFormData {
-  id?: number
-  anio: number
-  fecha: string
-  gerencia: string
-  usuarioId: number
-  semana: number
+  anio: number;
+  fecha: string;
+  gerencia: string;
+  semana: number;
+  createdAt?: string;
+  createdAtFormatted?: string;
+  id?: number;
+  updatedAt?: string;
+  updatedAtFormatted?: string;
+  usuario?: IUser;
+  usuarioId?: number;
 }
