d4a4afd2cd37382ad299784df0f8151b71ce22ee
Sahid Alejandro <sahidaledev@gmail.com>
2026-03-12 13:36:41 -0600

    feat(): agregar service error en corrección

--- src/features/correction/composables/index.ts
+++ src/features/correction/composables/index.ts
@@ -1,4 +1,3 @@
 export { useRecordCorrection } from './useRecordCorrection';
 export { useCorrectionValidation } from './useCorrectionValidation';
 export { useCorrectionData } from './useCorrectionData';
-export { useCorrectionErrorHandler } from './useCorrectionErrorHandler';
--- src/features/correction/composables/useCorrectionErrorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { useErrorHandler } from '@/shared/composables/useErrorHandler'
-import { CORRECTION_ERROR_MESSAGES, type CorrectionErrorType } from '../constants/errorMessages'
-
-export function useCorrectionErrorHandler() {
-  return useErrorHandler<CorrectionErrorType>(
-    CORRECTION_ERROR_MESSAGES,
-    'Correction'
-  )
-}
