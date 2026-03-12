c86b13bbc4df412a987e0cdcbd659f1177883466
Sahid Alejandro <sahidaledev@gmail.com>
2026-03-12 13:40:14 -0600

    feat(): eliminar correctionErrorHandler y sus referencias

--- src/features/correction/constants/errorMessages.ts
+++
@@ -1,8 +0,0 @@
-import { BASE_ERROR_MESSAGES } from '@/shared/constants/base-error-messages'
-
-export const CORRECTION_ERROR_MESSAGES = {
-  CORRECTION_SUBMIT_FAILED: 'Error al enviar la solicitud de corrección',
-  ...BASE_ERROR_MESSAGES
-} as const
-
-export type CorrectionErrorType = keyof typeof CORRECTION_ERROR_MESSAGES
--- src/features/correction/constants/index.ts
+++ src/features/correction/constants/index.ts
@@ -2,8 +2,3 @@ export {
   TYPE_LABELS,
   VALIDATION_MESSAGES,
 } from './correction.constants'
-
-export { 
-  CORRECTION_ERROR_MESSAGES, 
-  type CorrectionErrorType 
-} from './errorMessages'
