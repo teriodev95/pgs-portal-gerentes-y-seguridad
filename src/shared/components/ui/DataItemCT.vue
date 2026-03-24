eeb5a8f9dba73c8c64577d4b5ea77acadd7241d8
Sahid Alejandro <sahidaledev@gmail.com>
2026-03-24 15:34:57 -0600

    feat(): add collapsible details to weekly closing summary

---
+++ src/shared/components/ui/DataItemCT.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TextCT from './TextCT.vue';
+
+interface Props {
+  label: string
+  value: string | number
+  color?: 'blue' | 'yellow' | 'green' | 'gray'
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex flex-col items-start justify-between p-4 border border-blue-200 rounded-lg bg-blue-100">
+    <TextCT variant="primary">{{ label }}</TextCT>
+    <TextCT variant="secondary">{{ value }}</TextCT>
+  </div>
+</template>
